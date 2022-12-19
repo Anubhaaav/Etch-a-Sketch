afff6545e780db4adbbec5e3ab7f618c3710a6a3
Anubhaaav <anubhavbarooah9@gmail.com>
2022-12-19 15:49:14 +0530

    Styled the js and css. Button yet to be designed

---
+++ script.js
@@ -0,0 +1,18 @@
+const containerDiv = document.querySelector("#container");
+
+function makeRows(rows, columns) {
+    containerDiv.style.setProperty("--grid-rows", rows);
+    console.log(containerDiv);
+    containerDiv.style.setProperty("--grid-columns", columns);
+    for(i = 0; i< (rows * columns); i++){
+        let square = document.createElement("div");
+        //square.innerText = (i + 1);
+        containerDiv.appendChild(square).className = "grid-item";
+        square.addEventListener("mouseover", () => {
+            square.style.backgroundColor = "black";
+            console.log(square);
+        })
+    }
+}
+
+makeRows(14, 16);
