a5a745a5dab6102a196bf7f217118a4b79e249f6
Anubhaaav <anubhavbarooah9@gmail.com>
2022-12-20 15:46:38 +0530

    Entered the button, grid size limit and also used prompt

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 const containerDiv = document.querySelector("#container");
+const buttonEl = document.getElementById("btn");
+
+buttonEl.onclick = ()=>{
+   var a = prompt("Enter Grid size");
+   if (a <= 100){
+   makeRows(a,a);
+   }
+   else {
+    alert("Enter size below 100");
+   }
+}
+
+
 
 function makeRows(rows, columns) {
     containerDiv.style.setProperty("--grid-rows", rows);
@@ -15,4 +28,3 @@ function makeRows(rows, columns) {
     }
 }
 
-makeRows(14, 16);
